3b25d3798b014475b51653e6950e0fd84e91bbf4
Vitaliy Kholyavin <vkholyavin@yandex-team.ru>
2017-08-21 15:32:24 +0300

    MOBDEVAUTH-3426 Fix returning from browser to application (#44)

--- lib/src/main/java/com/yandex/authsdk/internal/BrowserLoginActivity.java
+++ lib/src/main/java/com/yandex/authsdk/internal/BrowserLoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -17,28 +18,22 @@ public class BrowserLoginActivity extends Activity {
 
     public static final String EXTRA_BROWSER_PACKAGE_NAME = "com.yandex.authsdk.internal.EXTRA_BROWSER_PACKAGE_NAME";
 
-    public static final String STATE_LOADING_STATE = "com.yandex.authsdk.STATE_LOADING_STATE";
+    private final Handler handler = new Handler();
 
-    private enum State {
-        INITIAL, TOKEN_REQUESTED, TOKEN_LOADED;
-    }
+    private final Runnable finishRunnable = () -> {
+        setResult(RESULT_CANCELED);
+        finish();
+    };
 
     @SuppressWarnings("NullableProblems") // onCreate
     @NonNull
     private ExternalLoginHandler loginHandler;
 
-    @NonNull
-    private State state = State.INITIAL;
-
     @Override
     protected void onNewIntent(final Intent intent) {
         super.onNewIntent(intent);
 
-        final Uri data = intent.getData();
-        if (data != null) {
-            parseTokenFromUri(data);
-        }
-        state = State.TOKEN_LOADED;
+        parseTokenFromUri(intent.getData());
     }
 
     @Override
@@ -49,36 +44,24 @@ public class BrowserLoginActivity extends Activity {
 
         loginHandler = new ExternalLoginHandler(new PreferencesHelper(this), () -> UUID.randomUUID().toString());
 
-        if (options == null) {
-            parseTokenFromUri(getIntent().getData());
-            return;
-        }
-
         if (savedInstanceState == null) {
             final Intent browserIntent = new Intent(Intent.ACTION_VIEW);
-            browserIntent.setPackage(getIntent().getStringExtra(EXTRA_BROWSER_PACKAGE_NAME)); // FIXME Crash if flag "Don't keep activities" checked
+            browserIntent.setPackage(getIntent().getStringExtra(EXTRA_BROWSER_PACKAGE_NAME));
             browserIntent.setData(Uri.parse(loginHandler.getUrl(options.getClientId())));
             startActivity(browserIntent);
-            state = State.INITIAL;
-        } else {
-            state = State.values()[savedInstanceState.getInt(STATE_LOADING_STATE)];
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        if (state == State.TOKEN_REQUESTED) {
-            finish();
-        }
-        state = State.TOKEN_REQUESTED;
+        handler.post(finishRunnable);
     }
 
     @Override
-    protected void onSaveInstanceState(@NonNull final Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(STATE_LOADING_STATE, state.ordinal());
+    protected void onPause() {
+        handler.removeCallbacks(finishRunnable);
+        super.onPause();
     }
 
     private void parseTokenFromUri(@NonNull final Uri data) {
