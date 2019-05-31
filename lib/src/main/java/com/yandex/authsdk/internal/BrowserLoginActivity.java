a235678a252a6304114e81ad99e1cc39ee139329
Vadik Sirekanyan <sirekanyan@gmail.com>
2019-05-31 13:06:35 +0300

    Removed finishRunnable to fix issue yandexmobile/yandex-login-sdk-android#4

--- lib/src/main/java/com/yandex/authsdk/internal/BrowserLoginActivity.java
+++ lib/src/main/java/com/yandex/authsdk/internal/BrowserLoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -12,20 +11,11 @@ import com.yandex.authsdk.YandexAuthOptions;
 
 import java.util.UUID;
 
-import static com.yandex.authsdk.internal.Constants.EXTRA_LOGIN_HINT;
 import static com.yandex.authsdk.internal.Constants.EXTRA_OPTIONS;
-import static com.yandex.authsdk.internal.Constants.EXTRA_UID_VALUE;
 
 public class BrowserLoginActivity extends Activity {
 
     public static final String EXTRA_BROWSER_PACKAGE_NAME = "com.yandex.authsdk.internal.EXTRA_BROWSER_PACKAGE_NAME";
-
-    private final Handler handler = new Handler();
-
-    private final Runnable finishRunnable = () -> {
-        setResult(RESULT_CANCELED);
-        finish();
-    };
 
     @SuppressWarnings("NullableProblems") // onCreate
     @NonNull
@@ -57,19 +47,8 @@ public class BrowserLoginActivity extends Activity {
             browserIntent.setPackage(getIntent().getStringExtra(EXTRA_BROWSER_PACKAGE_NAME));
             browserIntent.setData(Uri.parse(loginHandler.getUrl(getIntent())));
             startActivity(browserIntent);
+            finish();
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        handler.post(finishRunnable);
-    }
-
-    @Override
-    protected void onPause() {
-        handler.removeCallbacks(finishRunnable);
-        super.onPause();
     }
 
     private void parseTokenFromUri(@NonNull final Uri data) {
