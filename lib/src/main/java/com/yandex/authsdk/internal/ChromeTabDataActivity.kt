4bfb8efddce81549eacbf236cf8613215308cb16
Dmitry Svirikhin <dmitry.svirikhin@gmail.com>
2023-10-02 15:46:24 +0200

    MOBDEVAUTH-9236: Additional security checks

--- lib/src/main/java/com/yandex/authsdk/internal/ChromeTabDataActivity.kt
+++ lib/src/main/java/com/yandex/authsdk/internal/ChromeTabDataActivity.kt
@@ -3,16 +3,31 @@ package com.yandex.authsdk.internal
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 
 internal class ChromeTabDataActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        intent.extras?.let { extras ->
+            if (!extras.isEmpty) {
+                Log.e(TAG, "Extras in this intent mustn't exists due to security reasons")
+                extras.keySet().forEach { intent.removeExtra(it) }
+            }
+        }
+
         val intent = Intent(this, ChromeTabLoginActivity::class.java).apply {
             data = intent.data
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
+
         startActivity(intent)
         finish()
+    }
+
+    internal companion object {
+
+        val TAG: String = ChromeTabDataActivity::class.java.simpleName
     }
 }
