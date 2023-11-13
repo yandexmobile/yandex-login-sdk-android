3cd96f804985d0e8e5304feae9b540133acc5657
Dmitry Svirikhin <dmitry.svirikhin@gmail.com>
2023-11-13 14:43:00 +0100

    Login sdk: Remove outdated class

--- lib/src/main/java/com/yandex/authsdk/YandexAuthAccount.kt
+++
@@ -1,9 +0,0 @@
-package com.yandex.authsdk
-
-data class YandexAuthAccount(
-    val uid: Long,
-    val primaryDisplayName: String,
-    val secondaryDisplayName: String?,
-    val isAvatarEmpty: Boolean,
-    val avatarUrl: String?
-)
